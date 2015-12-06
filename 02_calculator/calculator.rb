2c9111839bad7443dc0d4a57d1d57b66108ee448
IhorV <ihor.venhliuk@gmail.com>
2015-12-06 19:52:37 +0200

    Create file calculator.rb and makes functions add, substact, sum

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,17 @@
+def add(p1, p2)
+  p1 + p2
+end
+
+def subtract(p1, p2)
+  (p1 - p2).abs
+end
+
+def sum(arr)
+  if arr == []
+    arr.join(",").to_i
+   else
+    arr.inject{|x, sum| x + sum}
+  end
+end
+
+
