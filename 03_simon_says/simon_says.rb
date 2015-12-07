77919e06b61fe8e8c727451e7532bd14fb4e6f7f
IhorV <vidioelf@gmail.com>
2015-12-07 18:01:49 +0200

    I've got a good practice with this stuff

---
+++ 03_simon_says/simon_says.rb
@@ -0,0 +1,41 @@
+def echo(str)
+  str
+end
+
+def shout(str)
+  str.upcase
+end
+
+def repeat(str, counter=2)
+  emp_arr = []
+  counter.times { emp_arr << str }
+  emp_arr.join(" ")
+end
+
+def start_of_word(str, hmn)
+  str[0..hmn-1]
+end
+
+def first_word(str)
+  arr = str.split(" ")
+  arr[0]
+end
+
+def titleize(str)
+  arr = str.split(" ")
+  arr_end = []
+  # small_words = ["and", "over", "the"]
+
+  for i in arr
+    if (i == arr[0]) ||
+       (i != "and") &&
+       (i != "over" ) &&
+       (i != "the") ; i.capitalize!; arr_end << i
+    else
+      arr_end << i
+    end
+  end
+  arr_end.join(" ")
+end
+
+puts titleize("the war and pease over")
